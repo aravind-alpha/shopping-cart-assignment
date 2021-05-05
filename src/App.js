901ea819217b12c26dd39da550c4bf034f42446b
Aravind <webdev.aravind@gmail.com>
2021-05-05 12:45:35 +0530

    updated application base files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "@reach/router";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import Layout from "./components/Layout/Layout";
+import reducer from "./redux/reducer";
+import "./App.css";
 
 const App = () => {
-  return <h1>Hello, I am rendered!</h1>;
+  const store = createStore(reducer, composeWithDevTools());
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <div>
+          <Layout>
+            <Router></Router>
+          </Layout>
+        </div>
+      </Provider>
+    </React.StrictMode>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
