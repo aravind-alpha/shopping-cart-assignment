15357c4ed917d3941c2bb3e9ac793e8b91c522f3
Aravind <webdev.aravind@gmail.com>
2021-05-06 13:44:40 +0530

    update App Layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import Layout from "./components/Layout/Layout";
 import reducer from "./redux/reducer";
+import Layout from "./components/Layout/Layout";
 import "./App.css";
 
 const App = () => {
   const store = createStore(reducer, composeWithDevTools());
   return (
     <React.StrictMode>
-      <Provider store={store}>
-        <div>
-          <Router>
-            <Layout>
-              <Switch>
-                <Route path="/" />
-              </Switch>
-            </Layout>
-          </Router>
-        </div>
-      </Provider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <Layout />
+        </Provider>
+      </BrowserRouter>
     </React.StrictMode>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
