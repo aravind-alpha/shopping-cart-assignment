823b9b2cf10d52a658569e902b957f5f853c9515
Aravind <webdev.aravind@gmail.com>
2021-05-10 20:00:47 +0530

    update: Completed Products Page

--- src/components/Layout/Routes.js
+++ src/components/Layout/Routes.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../../pages/Home/Home";
 import Login from "../../pages/Login";
 import Register from "../../pages/Register";
+import Product from "../../pages/Product";
 
 const Routes = () => {
   return (
@@ -10,6 +11,7 @@ const Routes = () => {
       <Route path="/" exact component={Home} />
       <Route path="/login" exact component={Login} />
       <Route path="/register" exact component={Register} />
+      <Route path="/products" exact component={Product} />
     </Switch>
   );
 };
