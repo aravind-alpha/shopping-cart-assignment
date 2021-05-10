6b8efafcefdf48c09673c54da657600e857ccb53
Aravind <webdev.aravind@gmail.com>
2021-05-10 20:25:41 +0530

    perf: add dynamic imports and code splitting

--- src/components/Layout/Routes.js
+++ src/components/Layout/Routes.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
-import Home from "../../pages/Home/Home";
-import Login from "../../pages/Login";
-import Register from "../../pages/Register";
-import Product from "../../pages/Product";
+
+const Home = lazy(() => import("../../pages/Home/Home"));
+const Login = lazy(() => import("../../pages/Login"));
+const Register = lazy(() => import("../../pages/Register"));
+const Product = lazy(() => import("../../pages/Product"));
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/register" exact component={Register} />
-      <Route path="/products" exact component={Product} />
-    </Switch>
+    <Suspense fallback={<h2 style={{ textAlign: "center" }}>Loading...</h2>}>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/products" exact component={Product} />
+      </Switch>
+    </Suspense>
   );
 };
 
