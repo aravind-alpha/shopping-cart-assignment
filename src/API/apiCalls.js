a56c24ee41c93118ed5b282187ec2246fde7d324
Aravind <webdev.aravind@gmail.com>
2021-05-10 10:39:17 +0530

    update: server config and API endpoints

--- src/API/apiCalls.js
+++ src/API/apiCalls.js
@@ -1,7 +1,5 @@
-const URL = "http://localhost:5000";
-
 export default {
-  banners: () => fetch(`${URL}/banners`).then((res) => res.data),
-  getCategories: () => fetch(`${URL}/categories`).then((res) => res.data),
-  getProducts: () => fetch(`${URL}/products`).then((res) => res.data),
+  bannersURL: "http://localhost:5000/banners",
+  productsURL: "http://localhost:5000/products",
+  categoriesURL: "http://localhost:5000/categories",
 };
