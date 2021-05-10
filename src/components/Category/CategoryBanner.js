1aa0eb15949811a305f427f2d082714d725a6b7f
Aravind <webdev.aravind@gmail.com>
2021-05-10 15:54:25 +0530

    added: Category Component

---
+++ src/components/Category/CategoryBanner.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./CategoryBanner.css";
+
+const CategoryBanner = (props) => {
+  const { category } = props;
+
+  return (
+    <div className="banner-component">
+      <div className="image-container">
+        <img
+          loading="lazy"
+          src={
+            require(`../../../static/images/category/${category.imageUrl}`)
+              .default
+          }
+          alt={category.name}
+          className="banner-image"
+          width="100%"
+        />
+      </div>
+      <div className="content">
+        <h2 className="heading">{category.name}</h2>
+        <p className="desc">{category.description}</p>
+        <Link to="/products" role="link" className="link-button">
+          Explore {category.key}
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryBanner;
